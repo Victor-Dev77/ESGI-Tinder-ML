00985382a03aa8ef3c85800b50a4c2e6945cac53
Victor <deyanovitch77580@gmail.com>
2021-01-26 19:03:40 +0100

    Scenario Deepfake or not

--- tinder-ml.py
+++ tinder-ml.py
@@ -1,9 +1,10 @@
 import numpy as np
 from math import *
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 def load_data(filename):
     f = open(filename)
@@ -26,23 +27,43 @@ def accuracy(confusion_matrix):
    sum_of_all_elements = confusion_matrix.sum()
    return diagonal_sum / sum_of_all_elements
 
+def showLearningCurve(mlp):
+    fig, ax = plt.subplots()
+    ax.plot(mlp.loss_curve_)
+    plt.yscale('log')
+    fig.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
     # Changer nom du fichier entre dataset_images et myfilename
 
-    #Ligne 1 / 2 / 9 a supprimer car 756 pixel au lieu de 784
-    inputs, desired = load_data('csv_deepfake.csv') #myfilename #csv_deepfake
-    inputs /= 255.0 #On normalise les entrées
+    deepfake_inputs, deepfake_desired = load_data('csv_deepfake.csv') #myfilename #csv_deepfake
+    deepfake_inputs /= 255.0 #On normalise les entrées
 
-    testinput, testdesired = load_data('csv_images.csv') #csv_images
-    testinput /= 255.0
+    res_inputs_test, res_output_test = load_data('csv_images.csv') #csv_images
+    res_inputs_test /= 255.0
 
-    #On sépare ensemble d'apprentissage et ensemble de test (ensemble = data)
-    TEST_PERCENTAGE = 30 #30
-    training_size = int(len(inputs) * (1.0 - TEST_PERCENTAGE / 100.0))
-    train_inputs, train_outputs = inputs[:training_size], desired[:training_size] #On coupe le jeu de données en 2
-    test_inputs, test_outputs = inputs[training_size:], desired[training_size:]
+    deepfake = False
+    TEST_PERCENTAGE = 100
 
-    X_train, X_test, y_train, y_test = train_test_split(inputs, desired)
+    inputs_test = []
+    outputs_test = []
+    train_inputs, train_outputs = [], []
+    if deepfake:
+        inputs_test = res_inputs_test
+        outputs_test = res_output_test
+        train_inputs, train_outputs = deepfake_inputs, deepfake_desired
+    else:
+        #On sépare ensemble d'apprentissage et ensemble de test (ensemble = data)
+        TEST_PERCENTAGE = 30 #30
+        training_size = int(len(deepfake_inputs) * (1.0 - TEST_PERCENTAGE / 100.0))
+        train_inputs, train_outputs = deepfake_inputs[:training_size], deepfake_desired[:training_size] #On coupe le jeu de données en 2
+        test_inputs, test_outputs = deepfake_inputs[training_size:], deepfake_desired[training_size:]
+        inputs_test = test_inputs
+        outputs_test = test_outputs
+
+    
+    X_train, X_test, y_train, y_test = train_test_split(deepfake_inputs, deepfake_desired)
 
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -57,26 +78,19 @@ if __name__ == "__main__":
     #mlp.fit(train_inputs, train_outputs)
 
 
-    mlp = MLPClassifier(hidden_layer_sizes=(13, 6,),
+    mlp = MLPClassifier(hidden_layer_sizes=(100, 50,),
                         solver='sgd',
+                        activation = 'logistic',
                         max_iter=2000,
-                        learning_rate_init=0.01,
+                        shuffle=True,
+                        learning_rate='constant',
+                        learning_rate_init=0.15,
                         momentum=0.9,
                         n_iter_no_change=10)
     #MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
     #mlp.fit(X_train,y_train)
+    print(len(train_inputs))
     mlp.fit(train_inputs, train_outputs)
-
-    deepfake = False
-    inputs_test = []
-    outputs_test = []
-    if deepfake:
-        inputs_test = testinput
-        outputs_test = testdesired
-    else:
-        inputs_test = test_inputs
-        outputs_test = test_outputs
-
 
     np.random.shuffle(inputs_test)
     predictions = mlp.predict(inputs_test)
@@ -97,17 +111,22 @@ if __name__ == "__main__":
     print(f"#Score de test : {round(learning_score * 100)}%")
 
     print(confusion_matrix(outputs_test, predictions, labels=[0, 1]))
-    print(classification_report(outputs_test, predictions))
+    print(classification_report(outputs_test, predictions, zero_division=0))
 
     #Evaluer sur l'ensemble de test la qualité de mon modèle
-    learning_score = mlp.score(inputs, desired)
+    learning_score = mlp.score(deepfake_inputs, deepfake_desired)
     print(f"Score d'apprentissage input desired : {round(learning_score * 100)}%")
 
     # Prediction
-    arr = [inputs[9]]
-    res = [desired[9]]
+    arr = [deepfake_inputs[9]]
+    res = [deepfake_desired[9]]
     prediction = mlp.predict(arr)
     print(f"Prediction 10e image: {prediction} -> res attendu: {res}")
 
     cm = confusion_matrix(prediction, res)
     print(f"Accuracy of MLPClassifier : {accuracy(cm)}")
+
+    res = accuracy_score(outputs_test, predictions)
+    print(f"accurancy --> {round(res * 100)}%")
+
+    #showLearningCurve(mlp)
